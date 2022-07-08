408ba1d50b01e41f804202f6ef4bee24af1bad77
Superb <staret.vision@gmail.com>
2022-07-08 22:54:14 +0900

    Add Converters folder Add DoubleToIntConverter Test Maui samples

---
+++ src/prototype/MarketClosingMauiPrototype/MarketClosingMauiPrototype/Converters/DoubleToIntConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketClosingMauiPrototype.Converters
+{
+    public class DoubleToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string strParam = parameter as string;
+            double multiplier = 1;
+
+            if (!string.IsNullOrEmpty(strParam))
+            {
+                Double.TryParse(strParam, out multiplier);
+            }
+            return (int)Math.Round((double)value * multiplier);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string strParam = parameter as string;
+            double divider = 1;
+
+            if (!string.IsNullOrEmpty(strParam))
+            {
+                Double.TryParse(strParam, out divider);
+            }
+
+            return (int)value / divider;
+        }
+    }
+}
